ebba3dd055d32d795c8576cdcf8a28c96420a166
G M Bueno <guilherme.bueno@leafagriculture.com.br>
2020-08-21 02:07:15 -0300

    exports and reads csv

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 start = dt.datetime(2010, 1, 1)
-end = dt.datetime(2019,12,31)
+end = dt.datetime(2020,12,31)
 
 df = web.DataReader('TSLA', 'yahoo', start, end)
-print(df.head())
+df.to_csv('tsla.csv')
+
+# we use parse_dates and index_col so date is an index (and it's column 0)
+df  = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+# print(df.head())
+
+fig, ax = plt.subplots()
+ax.set_ylabel('US Dollars')
+
+title = 'Tesla (NASDAQ: TSLA) Stock price'
+df['Adj Close'].plot(title=title, ax=ax)
+plt.show()
