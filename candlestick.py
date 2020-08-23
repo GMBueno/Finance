befc2f354b917b7e4e83d33d06a22229ae9d74b9
G M Bueno <guilherme.bueno@leafagriculture.com.br>
2020-08-22 23:46:16 -0300

    added candlestick

---
+++ candlestick.py
@@ -0,0 +1,15 @@
+import mplfinance as mpf
+import pandas as pd
+
+
+df= pd.read_csv('TSLA.csv',parse_dates=True, index_col=0)
+mpf.plot(df,
+        type='candle', # graph type (like bars, candlestick...)
+        style='charles', # graph color ('chales' is green for pos, red for neg)
+        title='NASDAQ: TSLA',
+        ylabel='Price (US Dollars)',
+        figratio=(25,10), # figure proportions. (25, 10) means x is 2.5 times y.
+        mav=100, # (x-day) moving average
+        ylabel_lower='Volume in millions',
+        volume=True
+        )
