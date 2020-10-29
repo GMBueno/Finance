117f0160172af932af7c55a7d02066aca8519d6f
G M Bueno <guilherme.bueno@leafagriculture.com.br>
2020-10-28 21:28:21 -0300

    refactor

--- src/investment_strategies.py
+++ src/investment_strategies.py
@@ -19,116 +19,50 @@ closes = df['Adj Close']
 # close = closes['2020-06-03']
 # print(close)
 
-'''
-1995 0
-1996 246
-1997 494
-1998 743
-1999 990
-2000 1236
-2001 1484
-2002 1731
-2003 1978
-2004 2228
-2005 2477
-2006 2726
-2007 2972
-2008 3217
-2009 3466
-2010 3712
-2011 3959
-2012 4208
-2013 4452
-2014 4700
-2015 4948
-2016 5196
-2017 5443
-2018 5690
-2019 5934
-2020 6181
-1995-2005 0-2476
-1996-2006 246-2725
-1997-2007 494-2971
-1998-2008 743-3216
-1999-2009 990-3465
-2000-2010 1236-3711
-2001-2011 1484-3958
-2002-2012 1731-4207
-2003-2013 1978-4451
-2004-2014 2228-4699
-2005-2015 2477-4947
-2006-2016 2726-5197
-2007-2017 2972-5442
-2008-2018 3217-5689
-2009-2019 3466-5933
-2010-2020 3712-6181
-'''
-
-def calc_i():
+def get_return(date1, date2):
     wallet = 100
-    day = '1'
-    wallets = []
-    for year in range(1995, 2011):
-        initial_year = str(year)
-        final_year = str(year + 10)
-        initial_price = find_first_price_of_year(initial_year)
-        final_price = find_first_price_of_year(final_year)
-        appreciation = final_price/initial_price
-        wallet = wallet*appreciation
-        wallets.append([initial_year, final_year, appreciation])
-    return wallets
+    date1 = get_next_valid_day(date1, try_current=True)
+    date2 = get_next_valid_day(date2, try_current=True)
+    initial_price = closes[date1]
+    final_price = closes[date2]
+    appreciation = 100 * ((final_price/initial_price)-1)
+    return appreciation
 
-def print_calc_i():
-    wallets = calc_i()
-    avg_appreciation = 0
-    for wallet in wallets:
-        initial_year = wallet[0]
-        final_year = wallet[1]
-        appreciation = wallet[2]
-        print(f'From {initial_year} to {final_year}: {appreciation*100:.2f}%')
-        avg_appreciation += appreciation
-    print(f'Average appreciation: {(avg_appreciation/len(wallets))*100:.2f}%')
+def avg_return():
+    riscs = []
+    risc = get_risc('1995-01-01', '2005-01-01')
 
-def find_first_price_of_year(year):
-    for day in range(1,9):
-        try:
-            price = closes[f'{year}-01-0{day}']
-            # print(f'found {year}-01-0{day}')
-            return price
-        except:
-            continue
+    appr = get_return('1995-01-01', '2005-01-01')
 
-def calc_sharpe():
-    # wallet = calc_i()[0]
-    test = np.array([1, 2, 3])
-    print(f'Risco: {test.std()}')
-
-# def calc_ii():
-#     for i, date, close in enumerate(closes):
-#         if i%30 == 0:
-#             invest
+    print(f'--> Risc: {risc*100:.2f}%')
+    print(f'--> Return: {appr:.2f}%')
 
 
-def get_next_valid_day(date):
+def get_next_valid_day(date, try_current=False):
     '''
     receives a date in this format: '1996-01-01' and output the next valid day,
     that is, a day that the stock exchange was opened and there is data.
     '''
+    next = 1
+    if try_current==True:
+        next = 0
     date = date.split('-')
     year = int(date[0])
     month = int(date[1])
     day = int(date[2])
-    for year in range(year,2021):
-        for month in range(month, 13):
-            for day in range(day+1,32):
-                date = make_date(year, month, day)
+    for yy in range(year,2021):
+        for mm in range(month, 13):
+            for dd in range(day+next,32):
+                date = make_date(yy, mm, dd)
+                # print('trying: ', date)
                 try:
                     price = closes[date]
                     return date
                 except:
                     continue
-            day = 1
-        month = 1
+            next = 0  # next iteration we have to try day 1.
+            day = 1  # next iteration starts at day 1
+        month = 1  # next iteration starts at month 1
     return 'THERE IS NO NEXT VALID DAY'
 
 def make_date(year, month, day):
@@ -169,78 +103,21 @@ def is_before(date1, date2):
 # print_calc_i()
 # calc_sharpe()
 
-def get_risc():
-    riscs = []
+def get_risc(date1, date2):
     prices = []
-    for i in range(0, 2476):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(246, 2725):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(494, 2971):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(743, 3216):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(990, 3465):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(1236, 3711):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(1484, 3958):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(1731, 4207):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(1978, 4451):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(2228, 4699):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(2477, 4947):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(2726, 5197):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(2972, 5442):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(3217, 5689):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(3466, 5933):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    prices = []
-    for i in range(3712, 6181):
-        prices.append((closes[i+1]/closes[i])-1)
-    riscs.append(np.array(prices).std())
-    return riscs
+    d1 = get_next_valid_day(date1, try_current=True)
+    d2 = get_next_valid_day(d1)
+    while is_before(d2, date2):
+        prices.append((closes[d2]/closes[d1])-1)
+        d1 = d2
+        d2 = get_next_valid_day(d1)
+    risc = np.array(prices).std()
+    return risc
 
-# year = 1995
-# wallets = calc_i()
-# for i, risc in enumerate(get_risc()):
-#     print(f'Years {year}-{year+10}')
-#     print(f'--> Risc: {risc*100:.2f}%')
-#     print(f'--> Return {wallets[i][2]*100:.2f}%\n')
-#     year = year +1
+date1 = '1995-01-01'
+date2 = '2005-01-01'
+risc = get_risc(date1, date2)
+appr = get_return(date1, date2)
+print(f'From {date1} to {date2}')
+print(f'--> Risc: {risc*100:.2f}%')
+print(f'--> Return: {appr:.2f}%')
