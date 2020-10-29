1e5d96ada4b1a2f200cf71ead4fb71e990247075
G M Bueno <guilherme.bueno@leafagriculture.com.br>
2020-10-28 22:19:09 -0300

    more refactoring

--- src/investment_strategies.py
+++ src/investment_strategies.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import pandas_datareader.data as web
 import numpy as np
+from pprint import pprint as pp
 
 '''
 Compare the following investment strategies, showing which had the highest
 profitability, lowest risk & highest Sharpe ratio for the Bovespa index history.
-i. invest R$X once and remove everything ten years later, also once;
+i. invest R$X once and withdraw everything ten years later, also once;
 ii. invest R$X a.m. for A years and withdraw the entire amount once, at the end;
 iii. invest R$X a.m. for A years and withdraw the amount gradually over B years;
 iv. Test the above strategies for some values, such as R$100.00 per month, in
@@ -28,15 +29,10 @@ def get_return(date1, date2):
     appreciation = 100 * ((final_price/initial_price)-1)
     return appreciation
 
-def avg_return():
-    riscs = []
-    risc = get_risc('1995-01-01', '2005-01-01')
-
-    appr = get_return('1995-01-01', '2005-01-01')
-
-    print(f'--> Risc: {risc*100:.2f}%')
-    print(f'--> Return: {appr:.2f}%')
-
+def get_first_close(year):
+    date = f'{year}-01-01'
+    date = get_next_valid_day(date)
+    return closes[date]
 
 def get_next_valid_day(date, try_current=False):
     '''
@@ -103,7 +99,7 @@ def is_before(date1, date2):
 # print_calc_i()
 # calc_sharpe()
 
-def get_risc(date1, date2):
+def get_risk(date1, date2):
     prices = []
     d1 = get_next_valid_day(date1, try_current=True)
     d2 = get_next_valid_day(d1)
@@ -111,13 +107,48 @@ def get_risc(date1, date2):
         prices.append((closes[d2]/closes[d1])-1)
         d1 = d2
         d2 = get_next_valid_day(d1)
-    risc = np.array(prices).std()
-    return risc
+    risk = 100 * np.array(prices).std()
+    return risk
 
-date1 = '1995-01-01'
-date2 = '2005-01-01'
-risc = get_risc(date1, date2)
-appr = get_return(date1, date2)
-print(f'From {date1} to {date2}')
-print(f'--> Risc: {risc*100:.2f}%')
-print(f'--> Return: {appr:.2f}%')
+def avg_return():
+    risk_ret = {}
+    rets = []
+    risks = []
+    start_year = 2009
+    for year in range(start_year,2011):
+        start_date = f'{year}-01-01'
+        end_date = f'{year+10}-01-01'
+        risk = get_risk(start_date, end_date)
+        ret = get_return(start_date, end_date)
+        rets.append(ret)
+        risks.append(risk)
+
+    for i in range(0, len(rets)):
+        print(f'Years {start_year+i}-{start_year+10+i}')
+        print(f'Bovespa {start_year+i}: {get_first_close(start_year+i):.0f}')
+        print(f'Bovespa {start_year+10+i}: {get_first_close(start_year+10+i):.0f}')
+        print(f'return: {rets[i]:.2f}%')
+        print(f'risk: {risks[i]:.2f}%\n')
+
+    sum_rets = 0
+    for ret in rets:
+        sum_rets = sum_rets + ret
+    avg_ret = sum_rets/len(rets)
+
+    sum_risks = 0
+    for risk in risks:
+        sum_risks = sum_risks + risk
+    avg_risk = sum_risks/len(risks)
+
+    print(f'avg return: {avg_ret:.2f}%')
+    print(f'avg risk: {avg_risk:.2f}%')
+
+avg_return()
+
+# date1 = '1995-01-01'
+# date2 = '2005-01-01'
+# risk = get_risk(date1, date2)
+# appr = get_return(date1, date2)
+# print(f'From {date1} to {date2}')
+# print(f'--> Risc: {risk*100:.2f}%')
+# print(f'--> Return: {appr:.2f}%')
